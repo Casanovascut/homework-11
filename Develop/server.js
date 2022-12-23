b89a038b00c2eaeb21e8d1fe88926df01a98e14a
Sebastian <smcasanova17@hotmail.com>
2022-12-23 15:59:38 -0500

    fixed page load

--- Develop/server.js
+++ Develop/server.js
@@ -19,13 +19,13 @@ app.use('/api', api);
 app.use(express.static('public'));
 
 // GET Route for homepage
-app.get('*', (req, res) =>
-    res.sendFile(path.join(__dirname, '/public/index.html'))
+app.get('/notes', (req, res) =>
+    res.sendFile(path.join(__dirname, '/public/notes.html'))
 );
 
 // GET Route for feedback page
-app.get('/notes', (req, res) =>
-    res.sendFile(path.join(__dirname, '/public/notes.html'))
+app.get('*', (req, res) =>
+    res.sendFile(path.join(__dirname, '/public/index.html'))
 );
 
 app.listen(PORT, () =>
